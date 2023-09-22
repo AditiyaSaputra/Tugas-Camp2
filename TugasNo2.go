052b2ffa058f9f91585d13673bef58fda5e78e36
Aditiya <aditiya.saputra@mhs.unsoed.ac.id>
2023-09-23 04:56:57 +0700

    Tugas Camp 2

--- TugasNo2.go
+++ TugasNo2.go
@@ -14,7 +14,7 @@ func main() {
 	// Cetak isi slice
 	fmt.Println("Isi awal slice:", slice)
 
-	// Menambahkan data ke dalam slice
+	// Menambahkan 3 data ke dalam slice
 	slice = append(slice, 60, 70, 80)
 
 	// Cetak isi slice setelah penambahan data
